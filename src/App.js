9cd43d8b0d17328c184b92fcdb36076e29e0f6ab
Dung Bui Viet <74093704+dbuiviet-xcomp@users.noreply.github.com>
2024-10-10 03:39:07 +0000

    add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
+
 function App() {
+
+  const courseGoals = [
+    {id: 'cg1', text: 'Finish the Course'},
+    {id: 'cg2', text: 'Learn all about the Course Main Topic'},
+    {id: 'cg3', text: 'Help other students in the Course Q&A'},
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <NewGoal />
+      <GoalList goals={courseGoals} />
     </div>
   );
+
+  // return (
+  //   // <div className="App">
+  //   //   <header className="App-header">
+  //   //     <img src={logo} className="App-logo" alt="logo" />
+  //   //     <p>
+  //   //       Edit <code>src/App.js</code> and save to reload.
+  //   //     </p>
+  //   //     <a
+  //   //       className="App-link"
+  //   //       href="https://reactjs.org"
+  //   //       target="_blank"
+  //   //       rel="noopener noreferrer"
+  //   //     >
+  //   //       Learn React
+  //   //     </a>
+  //   //   </header>
+  //   // </div>
+
+    
+  // );
 }
 
 export default App;
